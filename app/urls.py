b4ae40eca6b2890400cd7f827e182373d2204270
EmadGKamel <EmadGKamel@users.noreply.github.com>
2019-06-04 10:31:43 +0200

    small configs

--- app/urls.py
+++ app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
                path('profile/', views.profile, name='profile'),
                path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
                path('logout/', auth_views.LogoutView.as_view(template_name='app/logout.html'), name='logout'),
-               path('health/', views.health, name='health'),
                path('<str:pk>', views.detail, name='detail'),
+               path('health/', views.health, name='health'),
                ]
